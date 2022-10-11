8d1cc63b36d6e874f1a4268568f7175cde8627f8
revilo vader <oliver.cushman@mrjaffesclass.com>
2022-10-10 23:21:58 -0700

    work on rounding bug

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,6 +1,24 @@
-public class FizzBuzz
-{
+import static java.lang.Math.round;
+public class FizzBuzz {
     public static void main(String[] args) {
-        
+        double j;
+        double k;
+        int i = 1;
+        while (i < 100) {
+            j = i / 3;
+            k = i / 5;
+            
+            if (j == round(j) && k == round(k)) {
+                System.out.println("FizzBuzz");
+            } else if (j == round(j)) {
+                System.out.println("Fizz");
+            } else if (k == round(k)) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+            
+            ++i;
+        }
     }
 }
